d71f87957e1d56d2b13d17fef6f9d011454d0c97
Vinishreesho <shobanatamil2001@gmail.com>
2025-08-14 19:12:56 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Hero from '../compontents/Hero'
+import FeaturedDestination from '../compontents/FeaturedDestination'
+import ExclusiveOffers from '../compontents/ExclusiveOffers'
+import Testimonial from '../compontents/Testimonial'
+import NewsLetter from '../compontents/NewsLetter'
+
+function Home() {
+  return (
+    <>
+        <Hero />
+        <FeaturedDestination />
+        <ExclusiveOffers />
+        <Testimonial />
+        <NewsLetter />
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/hotelowner/Layout.jsx
@@ -0,0 +1,19 @@
+import Navbar from '../../compontents/hotelowner/Navbar'
+import Sidebar from '../../compontents/hotelowner/Sidebar'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => {
+  return (
+    <div className='flex flex-col h-full'>
+        <Navbar />
+        <div className='flex h-full'>
+            <Sidebar />
+            <div className='flex-1 p-4 pt-10 md:px-10 h-full'>
+                <Outlet />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Layout 
