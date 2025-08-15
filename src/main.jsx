faf1722d532b8e012f05877d84a37fbbccccb7c0
Vinishreesho <shobanatamil2001@gmail.com>
2025-08-15 12:28:00 +0530

    first-four

--- src/main.jsx
+++ src/main.jsx
@@ -12,11 +12,10 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
 }
 
-const BASE = import.meta.env.BASE_URL;
 
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl={BASE}>
-  <BrowserRouter basename={BASE}>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/myhotel/">
+  <BrowserRouter basename="/myhotel">
     <App />
   </BrowserRouter>
   </ClerkProvider>,
