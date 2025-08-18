da47b56437045c21b70c504864f50e2b9e28063f
Vinishreesho <shobanatamil2001@gmail.com>
2025-08-18 17:56:08 +0530

    last -first commit

--- src/compontents/ExclusiveOffers.jsx
+++ src/compontents/ExclusiveOffers.jsx
@@ -13,7 +13,7 @@ const ExclusiveOffers = () => {
             </button> */}
         </div>
 
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-12 gap-6'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-12 gap-6 '>
               {exclusiveOffers.map((item)=> (
                 <div key={item._id} style={{backgroundImage:`url(${item.image})`}} className='group relative flex flex-col items-start 
                 justify-between gap-1 pt-12 md:pt-18 px-4 rounded-xl text-white bg-no-repeat bg-cover bg-center'>
@@ -22,7 +22,7 @@ const ExclusiveOffers = () => {
                 <div>
                     <p className='text-2xl font-medium font-playfair'> {item.title}</p>
                     <p> {item.description}</p>
-                    <p className='text-xs text-white/70 mt-3'> Expires {item.expiryDate}</p>
+                    <p className='text-xs text-white/70 mt-3 mb-5'> Expires {item.expiryDate}</p>
                 </div>   
                      {/* <button className='flex items-center gap-2 font-medium cursor-pointer mt-4 mb-5'> 
                         View Offers 
